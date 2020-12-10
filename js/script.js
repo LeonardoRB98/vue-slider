527500d216aaafd6f1042d58790f0e38b98f6cf7
Leonardo <leonardo.ranaldo.b@gmail.com>
2020-12-10 11:34:38 +0100

    funzione al click sui dots

--- js/script.js
+++ js/script.js
@@ -4,8 +4,23 @@ let app = new Vue(
     data: {
       currentImg: 0,
       images: ['img/pink.jpg', 'img/blue.jpg', 'img/purple.jpg', 'img/red.jpg'],
+      autoPlay:''
+    },
+    created: function () {
+        this.autoplay = setInterval(() => {
+          this.nextImage();
+        },2000
+      );
     },
     methods: {
+      nextImageClick: () => {
+        clearInterval(this.autoPlay);
+        this.nextImage();
+      },
+      prevImageClick: () => {
+        clearInterval(this.autoPlay);
+        this.prevImage();
+      },
       nextImage: function() {
         this.currentImg++;
         if (this.currentImg == this.images.length) {
@@ -17,8 +32,10 @@ let app = new Vue(
         if (this.currentImg == -1) {
           this.currentImg = this.images.length - 1;
         }
+      },
+      dotsImage: function (dotsIndex) {
+        this.currentImg = dotsIndex;
       }
-
     }
   }
 );
